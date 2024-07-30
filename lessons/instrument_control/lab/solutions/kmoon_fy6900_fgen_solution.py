4f315e0ae5626cc2c7c7e192d350bbe35da52b94
Sam <sscondon@stanford.edu>
2024-07-30 09:48:59 +0700

    Proof read and cleaned the function generator notebooks and .py files for the IC/DAQ lab.

--- lessons/instrument_control/lab/solutions/kmoon_fy6900_fgen_solution.py
+++ lessons/instrument_control/lab/solutions/kmoon_fy6900_fgen_solution.py
@@ -8,7 +8,6 @@ channels = {1: 'M', 2: 'F'}
 def connect(port=0):
     """
     Connects to the function generator through the serial interface.
-    Confirms the connection by printing the instrument ID.
 
     :param port: Usb port number.
 
@@ -24,7 +23,7 @@ def connect(port=0):
 def set_wave_type(inst, channel, wave_type):
     """ Set the waveform type using the Basic Wave command.
 
-    :param inst: serial resource object corresponding to the waveform generator
+    :param inst: PyVisa resource object corresponding to the waveform generator
     :param wave_type: int, 1 for sine, 2 for square, etc
     """
     wave_map = {
@@ -34,6 +33,9 @@ def set_wave_type(inst, channel, wave_type):
         'Trapezoid': 3,
         'Impulse': 31,
     }
+    valid_wave_types = list(wave_map.keys())
+    if wave_type not in valid_wave_types:
+        raise ValueError(f'wave_type must be one of {valid_wave_types}')
     wave_type = wave_map[wave_type]
     command = f'W{channels[channel]}W{wave_type}'
     inst.write(command)
@@ -41,9 +43,9 @@ def set_wave_type(inst, channel, wave_type):
 def set_frequency(inst, channel, frequency):
     """ Set the waveform fundamental frequency.
     
-    :param inst: serial resource object corresponding to the waveform generator.
+    :param inst: PyVisa resource object corresponding to the waveform generator.
     :param channel: (int) Channel on which to change the waveform type.
-    :param frequency: COMPLETE THIS
+    :param frequency: (float) Frequency of the waveform in Hz.
     """
     command = f'W{channels[channel]}F{int(round(frequency * 1e6, 0)):010d}' 
     inst.write(command)
@@ -51,9 +53,9 @@ def set_frequency(inst, channel, frequency):
 def set_amplitude(inst, channel, amplitude):
     """ Set the waveform amplitude in peak-to-peak units.
 
-    :param inst: serial resource object corresponding to the waveform generator.
+    :param inst: PyVisa resource object corresponding to the waveform generator.
     :param channel: (int) Channel on which to change the waveform type.
-    :param amp: COMPLETE THIS
+    :param amp: (float) Amplitude of the waveform in volts peak-to-peak.
     """
     command = f'W{channels[channel]}A{round(amplitude, 4)}'
     inst.write(command)
@@ -61,15 +63,19 @@ def set_amplitude(inst, channel, amplitude):
 def set_offset(inst, channel, offset):
     """ Set the waveform DC offset.
 
-    :param inst: serial resource object corresponding to the waveform generator.
+    :param inst: PyVisa resource object corresponding to the waveform generator.
     :param channel: (int) Channel on which to change the waveform type.
-    :param offset: COMPLETE THIS
+    :param offset: (float) Waveform DC offset in Volts.
     """
     command = f'W{channels[channel]}O{round(offset, 4)}'
     inst.write(command)
 
 def set_channel_state(inst, channel, state):
-    """ Set the state of a waveform generator channel to ON or OFF
+    """ Set the state of a waveform generator channel to ON or OFF.
+
+    :param inst: PyVisa resource object corresponding to the waveform generator.
+    :param channel: (int) Waveform generator channel.
+    :param state: (str) 'ON' or 'OFF' specifying the desired state of the wavegen channel.
     """
     state_map = {
         'ON': 1,
@@ -87,15 +93,15 @@ def initialize(inst):
     """
     for channel in [1, 2]:
         set_channel_state(inst, channel, 'OFF')
-        sleep(0.2)
+        time.sleep(0.2)
         set_wave_type(inst, channel, 'SINE')
-        sleep(0.2)
+        time.sleep(0.2)
         set_frequency(inst, channel, 1)
-        sleep(0.2)
+        time.sleep(0.2)
         set_amplitude(inst, channel, 1)
-        sleep(0.2)
+        time.sleep(0.2)
         set_offset(inst, channel, 1)
-        sleep(0.2)
+        time.sleep(0.2)
 
 def create_log(channel, wave_type, freq, amp, offset):
     """ Create a string to log basic parameters of the FY6900 waveform generator output.
